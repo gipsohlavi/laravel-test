2e78528fb95a35415ed6e9f08d80a750a13fa99d
gipsohlavi <gipsohlavi@gmail.com>
2025-09-08 20:30:11 +0900

    breezeのコード群生成

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,22 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
-
-Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreatedController::class);
-
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
-->name('tweet.index');
-
-Route::post('/tweet/create' , \App\Http\Controllers\Tweet\CreatedController::class)
-->name('tweet.create');
-
-Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
-
-Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
-
-Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+require __DIR__.'/auth.php';
