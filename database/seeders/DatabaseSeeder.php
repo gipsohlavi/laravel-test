3c2bd5a64b2a1163b5738a88fd44ec28256a4c7f
gipsohlavi <gipsohlavi@gmail.com>
2025-09-08 21:00:06 +0900

    userID追加とSeederの変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,14 +10,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call([TweetsSeeder::class]);
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UsersSeeder::class,
+            TweetsSeeder::class
+        ]);
     }
 }
